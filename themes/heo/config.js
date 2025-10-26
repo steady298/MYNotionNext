d95b27d992aaa4bbceaac00f0bd20bd34246ce49
steady298 <115759238+steady298@users.noreply.github.com>
2025-10-26 20:05:59 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -50,9 +50,9 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://github.com/steady298',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL3: 'https://blog.steady298.com',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -116,10 +116,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: '联系我',
+  HEO_SOCIAL_CARD_TITLE_2: '交流合作',
+  HEO_SOCIAL_CARD_TITLE_3: '点击唤起Telegram',
+  HEO_SOCIAL_CARD_URL: 'https://t.me/steady298',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
